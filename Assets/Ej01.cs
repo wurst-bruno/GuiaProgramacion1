a25f713dfa22a3db47517cfa5e5d6d7b5e54a52e
wurst-bruno <101563441+wurst-bruno@users.noreply.github.com>
2022-03-22 15:57:10 -0300

    Exercise 01

---
+++ Assets/Ej01.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej01 : MonoBehaviour
+{
+    // Realizá un programa donde se cree una variable entera llamada num1, que inicialmente
+      //valdrá 0. Luego incrementá su valor en 2 y mostralo por pantalla. Después mostrá el
+     //resultado de multiplicarlo por sí mismo.
+     int num1= 0 
+    void Start()
+    {
+        num1 += 2;
+        Debug.Log(num1);
+        Debug.Log(num1 * num1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
