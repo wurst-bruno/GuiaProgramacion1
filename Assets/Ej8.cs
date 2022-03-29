170905f603beb8bab748a2be9dec6d80e6463bfe
wurst-bruno <101563441+wurst-bruno@users.noreply.github.com>
2022-03-29 15:55:48 -0300

    Ej 8 y 12
    gooood

---
+++ Assets/Ej8.cs
@@ -0,0 +1,53 @@
+﻿//    Crear una variable entera que pueda ser modificada desde el Inspector.
+//    Suponiendo que el primer día de la semana es el Domingo, mostrar el nombre del día
+//    correspondiente al valor de la variable “dia”. Si el día no está en el rango  permitido 
+//    debe emitir por pantalla el mensaje de error "El día ingresado no es válido". 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej8 : MonoBehaviour
+{
+    public int dia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (dia)
+        {
+            case 1:
+                Debug.Log("Hoy es Domingo");
+                break;
+
+            case 2:
+                Debug.Log("Hoy es Lunes");
+                break;
+            case 3:
+                Debug.Log("Hoy es Martes");
+                break;
+            case 4:
+                Debug.Log("Hoy es Miercoles");
+                break;
+            case 5:
+                Debug.Log("Hoy es Jueves");
+                break;
+            case 6:
+                Debug.Log("Hoy es Viernes");
+                break;
+            case 7:
+                Debug.Log("Hoy es Sabado");
+                break;
+            default:
+                Debug.Log("El día ingresado no es valido");
+                break;
+                   
+               
+ 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/EjEXTRA.cs
@@ -0,0 +1,58 @@
+﻿//12. En un casino de juegos se desea mostrar los mensajes respectivos por el puntaje  obtenido en el lanzamiento de tres dados de un cliente, de acuerdo a los siguientes  resultados:  
+//• Si los tres dados son seis, mostrar el mensaje “Excelente” 
+//• Si dos dados son seis, mostrar el mensaje “Muy bien”  
+//• Si un dado es seis, mostrar el mensaje “Regular”  
+//• Si ningún dado se obtiene seis, mostrar el mensaje “Insuficiente” 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjEXTRA : MonoBehaviour
+{
+    public int Dado1;
+    public int Dado2;
+    public int Dado3;
+
+    int cantSeis = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Dado1 ==6 )
+        {
+            cantSeis++;             
+        }
+        if (Dado2 ==6 )
+        {
+            cantSeis++;
+        }
+        if (Dado3 == 6)
+        {
+            cantSeis++;
+        }
+        switch (cantSeis)
+        {
+            case 1:
+                Debug.Log("Regular");
+                break;
+
+            case 2:
+                Debug.Log("Muy bien");
+                break;
+            case 3:
+                Debug.Log("Excelente");
+                break;
+            case 0:
+                Debug.Log("Insuficiente");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
